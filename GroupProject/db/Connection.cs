5e86193be89ead89a57c29538900aee3453c80a7
Rodrigo Garcia <rodgarcialima@gmail.com>
2018-08-03 08:05:11 -0400

    Connection string

---
+++ GroupProject/db/Connection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace GroupProject.db
+{
+    public static class Connection
+    {
+        public static SqlConnection NewConnection()
+        {
+            return new SqlConnection(ConfigurationManager.ConnectionStrings["RestaurantDbAzure"].ConnectionString);
+        }
+    }
+}
