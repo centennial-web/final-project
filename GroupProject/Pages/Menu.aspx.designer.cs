3e47cc63617d72536cb65b1ca500a4f9f86de208
FrancieleBrito <40642058+FrancieleBrito@users.noreply.github.com>
2018-08-08 20:48:08 -0700

    Datalist for Menu and ShoppingList

--- GroupProject/Pages/Menu.aspx.designer.cs
+++ GroupProject/Pages/Menu.aspx.designer.cs
@@ -38,14 +38,5 @@ namespace GroupProject.Pages {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.DataList dtlMenu;
-        
-        /// <summary>
-        /// btnMenu_AddtoCart control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnMenu_AddtoCart;
     }
 }
--- GroupProject/Pages/ShoppingCart.aspx.designer.cs
+++ GroupProject/Pages/ShoppingCart.aspx.designer.cs
@@ -40,13 +40,13 @@ namespace GroupProject.Pages {
         protected global::System.Web.UI.WebControls.Panel Panel2;
         
         /// <summary>
-        /// DataList1 control.
+        /// dtlShoppingList control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.DataList DataList1;
+        protected global::System.Web.UI.WebControls.DataList dtlShoppingList;
         
         /// <summary>
         /// Panel3 control.
